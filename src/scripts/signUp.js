5a5b16d8f058543c95323a1b3b632a217621745f
Yash raval <123889939+Yashraval366@users.noreply.github.com>
2025-02-26 20:55:51 +0530

    added registration api

---
+++ src/scripts/signUp.js
@@ -0,0 +1,42 @@
+const registerUser = async () => {
+    const form = document.getElementById("register-form");
+    const formData = new FormData(form);
+    const jsonData = Object.fromEntries(formData.entries());
+
+    document.getElementById("loading").style.display = "flex";
+
+    try {
+        const response = await fetch("http://localhost:5001/api/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(jsonData)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            setTimeout(() => {
+                document.getElementById("loading").style.display = "none";
+                document.getElementById("popup").style.display = "flex";
+            }, 3000);
+        
+            window.addEventListener("click", (event) => {
+                if (event.target === document.querySelector('body')) {
+                    document.getElementById("popup").style.display = "none";
+                }
+            });    
+        
+        } else {
+            alert("Error: " + data.message);
+        }
+    } catch (error) {
+        alert("Network Error! Please try again.");
+    } 
+};
+
+document.getElementById("register-form").addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    registerUser();
+});
