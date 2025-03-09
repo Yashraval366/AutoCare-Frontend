a2a2c39e1893851b7619f54dd8458658269402bc
Vaishnavi4924 <vaishnavipadhiyar13@gmail.com>
2025-03-09 18:43:10 +0530

    new code

---
+++ src/scripts/user.js
@@ -0,0 +1,38 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     const token = localStorage.getItem('authToken')
+
+//     console.log(token)
+
+//     const UserData = async() => {
+//         try {
+//             const response = await fetch('http://localhost:5001/api/users/getusers', {
+//                 method: "GET",
+//                 headers : {
+//                     "Authorization": `Bearer ${token}`,
+//                     "Content-Type": "application/json" 
+//                 }
+//             })
+
+//             if (!response.ok) {
+//                 throw new Error("Failed to fetch user data");
+//             }
+
+//             const data = await response.json();
+
+//             console.log(data);
+
+//             document.getElementById('user-name').innerHTML = data.User.username
+//         }
+//         catch(err) {
+//             console.log("Error: ",err)
+//         }
+//     }
+//     UserData();
+
+//     document.getElementById('logout').addEventListener('click', ()=>{
+//         localStorage.removeItem("authToken");
+//         window.location.href = "#";
+//         alert("Logged out successfully") 
+//     })
+// })
